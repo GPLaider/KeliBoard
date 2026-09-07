6632d06a0baac9aef677a269340ec21721a8e531
GPLaider <221012486+GPLaider@users.noreply.github.com>
2026-09-07 20:13:47 +0900

    Prepare KeliBoard 4.0.0 stable release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.github.gplaider.keliboard"
         minSdk = 21
         targetSdk = 36
-        versionCode = 4017
-        versionName = "4.0-ko-preview11"
+        versionCode = 4018
+        versionName = "4.0.0"
         ndk {
             abiFilters.clear()
             abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
